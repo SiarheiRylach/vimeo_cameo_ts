4eca61f7ee915cf50ed8acc206041d9cbb3cda7e
Siarhei_Rylach <Siarhei_Rylach@epam.com>
2018-02-12 12:46:03 +0300

    all is changed

--- step_definitions/hooks.ts
+++ step_definitions/hooks.ts
@@ -1,8 +1,8 @@
 import {browser} from "protractor";
 const {After} = require("cucumber");
 
-    After({tags: '@scroll-start-page'}, () => {
-        return browser.executeScript('window.scroll(0, 0)');
-    });
+After({tags: '@scroll-start-page'}, () => {
+    return browser.executeScript('window.scroll(0, 0)');
+});
 
 
